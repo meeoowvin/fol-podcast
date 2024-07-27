faf57e68310e27359211e40c901c33e2bfbd3ae6
Marvin John Salazar <mj.salazar.mj@gmail.com>
2024-07-27 23:45:25 +0800

    4

--- src/site-config.ts
+++ src/site-config.ts
@@ -3,10 +3,10 @@ export default {
   description:
     'Tulungan nyo ko mag isip ng description para dito',
   listen: {
-    spotify: 'https://open.spotify.com/show/6jKKAcpzGP02o4xBmXofjP',
+    spotify: 'https://www.facebook.com/',
     apple:
-      'https://podcasts.apple.com/us/podcast/mediocre-minds-podcast/id1637152861',
-    google: 'https://podcasts.google.com/feed/aHR0cHM6Ly9tZWRpb2NyZW1pbmRzLmdpdGh1Yi5pby9wb2RjYXN0L3Jzcy9mZWVkLnhtbA',
-    rss: 'https://mediominds.libsyn.com/rss',
+      'https://www.facebook.com/',
+    google: 'https://www.facebook.com/',
+    rss: 'https://www.facebook.com/',
   },
 }
